dfa328323fdea61492c9090174021a5ffc71547e
joeslack <joeslack_@hotmail.com>
2017-08-01 14:52:26 +0100

    init

---
+++ TestMD/TestMD/Pages/MasterDetail.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TestMD.Model;
+using Xamarin.Forms;
+
+namespace TestMD.Pages
+{
+    public partial class MasterDetail : MasterDetailPage
+    {
+        public MasterDetail()
+        {
+            InitializeComponent();
+
+            masterPage.ListView.ItemSelected += OnItemSelected;
+            masterPage.ListView2.ItemSelected += OnItemSelected;
+            masterPage.ListView3.ItemSelected += OnItemSelected;
+        }
+
+   
+
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as MasterPageItem;
+            if (item != null)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+                masterPage.ListView.SelectedItem = null;
+                masterPage.ListView2.SelectedItem = null;
+                masterPage.ListView3.SelectedItem = null;
+                IsPresented = false;
+            }
+        }
+    }
+}
---
+++ TestMD/TestMD/Pages/MasterPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestMD.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestMD.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterPage : ContentPage
+    {
+        public ListView ListView { get { return addnewlistView; } }
+        public ListView ListView2 { get { return currenttaskslistView; } }
+
+        public ListView ListView3 { get { return historictaskslistView; } }
+
+        public MasterPage()
+        {
+            InitializeComponent();
+
+
+            var addnewList = new List<MasterPageItem>();
+
+
+            addnewList.Add(new MasterPageItem
+            {
+                Title = "Create New To Do",
+                TargetType = (typeof(NewToDo))
+
+            });
+                 
+
+            addnewlistView.ItemsSource = addnewList;
+
+
+            var currenttasksList = new List<MasterPageItem>();
+
+            currenttasksList.Add(new MasterPageItem
+            {
+                Title = "Today",
+                TargetType = (typeof(CurrentToday))
+            });
+            currenttasksList.Add(new MasterPageItem
+            {
+                Title = "This Week",
+                TargetType = (typeof(CurrentWeek))
+            });
+            currenttasksList.Add(new MasterPageItem
+            {
+                Title = "All Open",
+                TargetType = (typeof(CurrentAll))
+            });
+
+            currenttaskslistView.ItemsSource = currenttasksList;
+
+
+            var historictasksList = new List<MasterPageItem>();
+
+
+            historictasksList.Add(new MasterPageItem
+            {
+                Title = "All",
+                TargetType = (typeof(HistoricAll))
+
+            });
+            historictaskslistView.ItemsSource = historictasksList;
+        }
+    }
+    }
